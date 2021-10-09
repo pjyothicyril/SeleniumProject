853c05f465bc6c5c52c49e10c7d6ec4ecd22cdfb
JyothiCyril <pjyothi.lakshmi@gmail.com>
2021-10-09 13:17:00 +0530

    First Commit - Review

---
+++ src/com/qa/selenium/FirstSeleniumProgram.java
@@ -0,0 +1,15 @@
+package com.qa.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSeleniumProgram {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+	
+
+	}
+
+}
